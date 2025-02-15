0b0521cf080e1701a463e6970ed39aa3b6e35ee0
Chandana Kara <155251095+chandana1299@users.noreply.github.com>
2025-02-15 05:35:15 +0530

    Delete api.py

--- api.py
+++
@@ -1,9 +0,0 @@
-from flask import Flask, make_response
-
-app = Flask(__name__)
-
-@app.route('/')
-def set_cookie():
-    response = make_response("Cookie Set")
-    response.set_cookie("my_cookie", "value", samesite="None", secure=True, httponly=True)
-    return response
